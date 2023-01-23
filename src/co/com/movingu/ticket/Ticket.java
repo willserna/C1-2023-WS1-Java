81838009158a69b6c185c92f015ec0af79dac756
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2023-01-23 00:10:51 -0500

    Class for Ticket package

---
+++ src/co/com/movingu/ticket/Ticket.java
@@ -0,0 +1,7 @@
+package co.com.movingu.ticket;
+
+//TO DO: Complete/Rewrite the class according to the requirements
+public class Ticket {
+
+    //TO DO: Add the necessary getters & setters u other extra function
+}
