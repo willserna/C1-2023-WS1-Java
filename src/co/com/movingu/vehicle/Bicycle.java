68d9f33c2464b2a353a54dfd436c6dfc0f56b14c
William Serna <williamsernac@hotmail.com>
2023-01-23 17:30:25 -0500

    Update Bicycle.java
    Bicycle class edited

--- src/co/com/movingu/vehicle/Bicycle.java
+++ src/co/com/movingu/vehicle/Bicycle.java
@@ -1,5 +1,9 @@
 package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
-public class Bicycle {
+public class Bicycle extends Vehicle {
+    private String bikeType;
+    public Bicycle(boolean available) {
+        super(available);
+    }
 }
