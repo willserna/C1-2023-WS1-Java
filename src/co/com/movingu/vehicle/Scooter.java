2abcea4923ab60b1837d3372d40180ae59a9c38e
William Serna <williamsernac@hotmail.com>
2023-01-23 17:31:06 -0500

    Update Scooter.java
    Scooter class edited

--- src/co/com/movingu/vehicle/Scooter.java
+++ src/co/com/movingu/vehicle/Scooter.java
@@ -2,6 +2,7 @@ package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
 public class Scooter extends Vehicle{
+    private int battery;
     public Scooter(boolean available) {
         super(available);
     }
