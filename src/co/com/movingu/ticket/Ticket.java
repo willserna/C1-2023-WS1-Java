0ef5f13781df76fc4f88d78a43cf27778dbb6946
William Serna <williamsernac@hotmail.com>
2023-01-23 20:43:53 -0500

    Update Ticket.java
    Ticket class complete

--- src/co/com/movingu/ticket/Ticket.java
+++ src/co/com/movingu/ticket/Ticket.java
@@ -1,7 +1,44 @@
 package co.com.movingu.ticket;
 
-//TO DO: Complete/Rewrite the class according to the requirements
-public class Ticket {
+import java.time.LocalDateTime;
 
-    //TO DO: Add the necessary getters & setters u other extra function
+
+
+public class Ticket {
+    String str1 = "T-";
+    protected String id;
+    protected LocalDateTime timeStart;
+    protected LocalDateTime timeEnd;
+    protected String status ;
+    protected boolean helmet;
+
+    protected float debt;
+
+    private Ticket(String id, LocalDateTime timeStart, LocalDateTime timeEnd, String status, boolean helmet, float debt) {
+        this.id = id;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+        this.status = status;
+        this.helmet = helmet;
+        this.debt = debt;
+    }
+
+
+
+    public String setId(int count) {
+
+        String str = String.valueOf(count);
+        this.id = str1 + str;
+        return id;
+    }
+    public LocalDateTime setTimeStart(LocalDateTime t) {
+
+        timeStart = t;
+        return timeStart;
+    }
+    public String setStatus(String status) {
+        this.status = status;
+        return status;
+    }
 }
+
