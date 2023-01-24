8d0b0434c46f90b8380b3d779457a1d5d6b61335
William Serna <williamsernac@hotmail.com>
2023-01-23 23:30:51 -0500

    Constructor for Vehicle types
    Added constructor to Bicycle and Scooter classes

--- src/co/com/movingu/vehicle/Bicycle.java
+++ src/co/com/movingu/vehicle/Bicycle.java
@@ -3,7 +3,9 @@ package co.com.movingu.vehicle;
 //TO DO: Complete/Rewrite the class according to the requirements
 public class Bicycle extends Vehicle {
     private String bikeType;
-    public Bicycle(boolean available) {
-        super(available);
+
+    public Bicycle(boolean available, String code, String color, boolean condition, String bikeType) {
+        super(available, code, color, condition);
+        this.bikeType = bikeType;
     }
 }
--- src/co/com/movingu/vehicle/Scooter.java
+++ src/co/com/movingu/vehicle/Scooter.java
@@ -3,7 +3,8 @@ package co.com.movingu.vehicle;
 //TO DO: Complete/Rewrite the class according to the requirements
 public class Scooter extends Vehicle{
     private int battery;
-    public Scooter(boolean available) {
-        super(available);
+    public Scooter(boolean available, String code, String color, boolean condition, int battery) {
+        super(available, code, color, condition);
+        this.battery = battery;
     }
 }
